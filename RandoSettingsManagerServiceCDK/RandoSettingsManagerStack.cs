4faa6f13c0a54427db56a7c48b2aee7594f4f0a9
Sean Dempsey <dempsey.sean@outlook.com>
2023-01-10 21:08:33 -0800

    Add alarming on 5xx errors

--- RandoSettingsManagerServiceCDK/RandoSettingsManagerStack.cs
+++ RandoSettingsManagerServiceCDK/RandoSettingsManagerStack.cs
@@ -1,6 +1,9 @@
 ﻿using Amazon.CDK;
+using Amazon.CDK.AWS.CloudWatch;
+using Amazon.CDK.AWS.CloudWatch.Actions;
 using Amazon.CDK.AWS.DynamoDB;
 using Amazon.CDK.AWS.Lambda;
+using Amazon.CDK.AWS.SNS;
 using Constructs;
 
 namespace RandoSettingsManagerServiceCDK
@@ -48,7 +51,32 @@ namespace RandoSettingsManagerServiceCDK
                 Handler = "RandoSettingsManagerService::RandoSettingsManagerService.Handlers::FunctionHandler",
                 FunctionName = "ManageSettings",
                 Timeout = Duration.Seconds(30),
+                LogRetention = Amazon.CDK.AWS.Logs.RetentionDays.THREE_MONTHS,
             });
+
+            Topic alarmTopic = new(this, "AlarmTopic", new TopicProps
+            {
+                DisplayName = "RandoSettingsManagerService Alarms",
+                TopicName = "RSMAlarms"
+            });
+
+            Metric _5xxCount = createSettings.Metric("Url5xxCount", new MetricOptions
+            {
+                Statistic = "Sum",
+                Period = Duration.Minutes(1)
+            });
+            Alarm _5xxAlarm = new(this, "ServerErrors", new AlarmProps
+            {
+                AlarmName = "RandoSettingsManagerService.ManageSettings.5xxErrors",
+                AlarmDescription = "Alarms when the function URL returns any 5xx errors for any 2 out of 5 minutes.",
+                Metric = _5xxCount,
+                Threshold = 1,
+                DatapointsToAlarm = 2,
+                EvaluationPeriods = 5,
+                TreatMissingData = TreatMissingData.NOT_BREACHING,
+                ComparisonOperator = ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD,
+            });
+            _5xxAlarm.AddAlarmAction(new SnsAction(alarmTopic));
 
             // calling createSettings.AddFunctionURL is a create-only operation, which is a problem because it fails subsequent deployments. do it by hand.
             new FunctionUrl(this, "SettingsLambdaURL", new FunctionUrlProps
